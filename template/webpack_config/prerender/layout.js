e4531d19722050591a764f8c64b3542086f8cbd3
Steve <stevenmathews.nz@gmail.com>
2016-05-19 15:58:52 +1200

    Add env as global variable

--- template/webpack_config/prerender/layout.js
+++ template/webpack_config/prerender/layout.js
@@ -1,7 +1,8 @@
-module.exports = [
-  '<div class="layout">',
-    '<h1>{{basename}}</h1>', // eslint-disable-line indent
-  '</div>',
-  '<div id="app">',
-  '</div>'
-].join('')
+module.exports = `
+  <div class="layout">
+    <h1>{{basename}}</h1>
+  </div>
+  <div id="app">
+  </div>
+  <script type="text/javascript">var env = "${process.env.NODE_ENV}"</script>
+`
